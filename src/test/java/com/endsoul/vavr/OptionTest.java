662aa5652a8b3315a03a07863adbd4a6df698419
endsoul <kkw855@gmail.com>
2021-04-05 15:57:25 +0900

    Option map

--- src/test/java/com/endsoul/vavr/OptionTest.java
+++ src/test/java/com/endsoul/vavr/OptionTest.java
@@ -13,4 +13,12 @@ class OptionTest {
     assertEquals(Option.of(null), Option.none());
     assertEquals(Option.of(10), Option.some(10));
   }
+
+  @Test
+  void map() {
+    assertEquals(Option.of(1).map(v -> v * 10).map(v -> v + 5), Option.some(15));
+    assertEquals(
+        Option.of("foo").flatMap(s -> Option.of((String) null)).map(s -> s.toUpperCase() + "bar"),
+        Option.none());
+  }
 }
